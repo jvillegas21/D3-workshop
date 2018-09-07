8ce226cfd92f988a197821009d8750f753c8f5a0
jvillegas21 <jmvegas21@gmail.com>
2018-09-07 13:22:43 -0700

    ScatterPlot
    Scatter plot with onClick function as well as updating state through getDerived...

--- src/Scatterplot.js
+++ src/Scatterplot.js
@@ -3,24 +3,44 @@ import * as d3 from "d3";
 import Axis from "./Axis";
 
 class Scatterplot extends Component {
-  xScale = d3
-    .scaleLinear()
-    .domain([0, 1])
-    .range([0, 400]);
+  state = {
+    xScale: d3
+      .scaleLinear()
+      .domain([1, 0])
+      .range([0, this.props.width]),
 
-  yScale = d3
-    .scaleLinear()
-    .domain([0, 1])
-    .range([0, 400]);
+    yScale: d3
+      .scaleLinear()
+      .domain([0, 1])
+      .range([0, this.props.height])
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    let { xScale, yScale } = state;
+
+    xScale.range([0, props.width]);
+    yScale.range([0, props.height]);
+  }
 
   render() {
     const { data, x, y } = this.props;
     return (
       <g transform={`translate(${x}, ${y})`}>
         {data.map(([x, y]) => (
-          <circle cx={this.xScale(x)} cy={this.yScale(y)} r={3} />
+          <circle cx={this.state.xScale(x)} cy={this.state.yScale(y)} r={3} />
         ))}
-        <Axis x={0} y={400} scale={(this.xScale, this.yScale)} type="Bottom" />
+        <Axis
+          x={this.props.width}
+          y={0}
+          scale={this.state.xScale}
+          type="Right"
+        />
+        <Axis
+          x={0}
+          y={this.props.height}
+          scale={this.state.xScale}
+          type="Bottom"
+        />
       </g>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import * as d3 from "d3";
 import Scatterplot from "./Scatterplot";
@@ -7,15 +7,36 @@ const data = d3.range(100).map(_ => [Math.random(), Math.random()]);
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <svg width="800" height="800">
-        <Scatterplot x={10} y={10} data={data} />
-      </svg>
-    </div>
-  );
+class App extends Component {
+  state = {
+    width: 500,
+    height: 500
+  };
+
+  onClick = () =>
+    this.setState({
+      width: this.state.width * 0.8,
+      height: this.state.height * 0.8
+    });
+
+  render() {
+    const { width, height } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <svg width="800" height="800" onClick={this.onClick}>
+          <Scatterplot
+            x={10}
+            y={10}
+            data={data}
+            width={width}
+            height={height}
+          />
+        </svg>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
